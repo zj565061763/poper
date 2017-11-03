496be735b75ceafd9bedff4f71c5ef1c5b4305da
zjun <565061763@qq.com>
2017-11-03 17:27:37 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
+++ app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
@@ -33,8 +33,8 @@ public class SimpleActivity extends AppCompatActivity
             }
         });
 
-        mPoper = new SDPoper(this);
-        mPoper.setContainer(fl_container) // 设置popview可以显示的容器范围，默认是Activity中id为android.R.id.content的容器
+        mPoper = new SDPoper(this)
+                .setContainer(fl_container) // 设置popview可以显示的容器范围，默认是Activity中id为android.R.id.content的容器
                 //.setMarginX(10) // 设置x轴需要偏移的值，大于0往右，小于0往左
                 //.setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
                 .setTrackTargetVisibility(true) // 设置是否跟随target的可见状态，默认true-跟随
