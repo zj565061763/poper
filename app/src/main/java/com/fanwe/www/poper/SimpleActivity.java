9256ceacb8f88bf535fd172943e1850b11eec086
zjun <565061763@qq.com>
2017-11-04 02:15:08 +0800

    重构

--- app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
+++ app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
@@ -15,7 +15,9 @@ public class SimpleActivity extends AppCompatActivity
 {
 
     private WeakReference<SDPoper> mPoper;
+
     private ViewGroup fl_container;
+    private TextView tv_target;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,6 +26,7 @@ public class SimpleActivity extends AppCompatActivity
         SDLibrary.getInstance().init(getApplication());
         setContentView(R.layout.act_simple);
         fl_container = (ViewGroup) findViewById(R.id.fl_container);
+        tv_target = (TextView) findViewById(R.id.tv_target);
 
         findViewById(R.id.tv_target).setOnClickListener(new View.OnClickListener()
         {
@@ -41,7 +44,7 @@ public class SimpleActivity extends AppCompatActivity
                 //.setMarginX(10) // 设置x轴需要偏移的值，大于0往右，小于0往左
                 //.setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
                 .setPopView(R.layout.view_pop) // 设置要popview，可以是布局id或者View对象
-                .setTarget(findViewById(R.id.tv_target)) // 设置要跟踪的目标View
+                .setTarget(tv_target) // 设置要跟踪的目标View
                 .attach(true); // true-依附目标view，false-移除依附
 
         mPoper = new WeakReference<>(poper);
