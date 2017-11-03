d6d3faee58f665ff80fcccd3b006ab63f6ebcae4
zjun <565061763@qq.com>
2017-11-04 01:13:10 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
+++ app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
@@ -38,7 +38,6 @@ public class SimpleActivity extends AppCompatActivity
                 .setContainer(fl_container) // 设置popview可以显示的容器范围，默认是Activity中id为android.R.id.content的容器
                 //.setMarginX(10) // 设置x轴需要偏移的值，大于0往右，小于0往左
                 //.setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
-                .setTrackTargetVisibility(true) // 设置是否跟随target的可见状态，默认true-跟随
                 .setPopView(R.layout.view_pop) // 设置要popview，可以是布局id或者View对象
                 .setTarget(findViewById(R.id.tv_target)); // 设置要跟踪的目标View
     }
@@ -179,7 +178,7 @@ public class SimpleActivity extends AppCompatActivity
             v.setVisibility(View.VISIBLE);
         } else
         {
-            v.setVisibility(View.INVISIBLE);
+            v.setVisibility(View.GONE);
         }
     }
 
