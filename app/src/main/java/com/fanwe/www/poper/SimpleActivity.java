ef8d01102780d07f166bb61b694763d8740da18e
zjun <565061763@qq.com>
2017-11-04 23:43:22 +0800

    demo重构

--- app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
+++ app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
@@ -38,9 +38,9 @@ public class SimpleActivity extends AppCompatActivity
 
         SDPoper poper = new SDPoper(this)
                 .setDebug(true)
-                .setContainer(fl_container) // 设置popview可以显示的容器范围，默认是Activity中id为android.R.id.content的容器
-                //.setMarginX(10) // 设置x轴需要偏移的值，大于0往右，小于0往左
-                //.setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
+//                .setContainer(fl_container) // 设置popview可以显示的容器范围，默认是Activity中id为android.R.id.content的容器
+//                .setMarginX(10) // 设置x轴需要偏移的值，大于0往右，小于0往左
+//                .setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
                 .setPopView(R.layout.view_pop) // 设置要popview，可以是布局id或者View对象
                 .setTarget(tv_target); // 设置要跟踪的目标View
 
