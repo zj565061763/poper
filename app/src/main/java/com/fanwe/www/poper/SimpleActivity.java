b1ae591ea93a26ec5e27a134caa950716b46bc24
zjun <565061763@qq.com>
2017-11-05 00:23:52 +0800

    重构

--- app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
+++ app/src/main/java/com/fanwe/www/poper/SimpleActivity.java
@@ -43,7 +43,8 @@ public class SimpleActivity extends AppCompatActivity
 //                .setMarginY(10) // 设置y轴方向的偏移量，大于0往下，小于0往上
                 .setPopView(R.layout.view_pop) // 设置要popview，可以是布局id或者View对象
                 .setPosition(SDPoper.Position.TopLeft) //左上角对齐
-                .setTarget(tv_target); // 设置要跟踪的目标View
+                .setTarget(tv_target) // 设置要跟踪的目标View
+                .attach(true); // //true-依附目标view，false-移除依附
 
         mPoper = poper;
     }
