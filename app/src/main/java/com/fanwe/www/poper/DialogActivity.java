90951920ff4092bf40b89b09371e40c24d8af6a1
zjun <565061763@qq.com>
2018-05-08 21:13:27 +0800

    demo

--- app/src/main/java/com/fanwe/www/poper/DialogActivity.java
+++ app/src/main/java/com/fanwe/www/poper/DialogActivity.java
@@ -1,21 +1,21 @@
 package com.fanwe.www.poper;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.fanwe.library.activity.SDBaseActivity;
 import com.fanwe.www.poper.dialog.TestDialog;
 
 /**
  * Created by Administrator on 2018/1/10.
  */
 
-public class DialogActivity extends AppCompatActivity
+public class DialogActivity extends SDBaseActivity
 {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void init(Bundle savedInstanceState)
     {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.act_dialog);
 
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener()
