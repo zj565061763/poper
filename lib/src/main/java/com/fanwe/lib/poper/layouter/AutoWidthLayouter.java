81e312b060df11ecc3023b3484fe0eed98d70b11
zjun <565061763@qq.com>
2018-05-09 15:36:16 +0800

    AutoWidthLayouter

--- lib/src/main/java/com/fanwe/lib/poper/layouter/AutoWidthLayouter.java
+++ lib/src/main/java/com/fanwe/lib/poper/layouter/AutoWidthLayouter.java
@@ -3,12 +3,12 @@ package com.fanwe.lib.poper.layouter;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class AutoHeightLayouter implements PopLayouter
+public class AutoWidthLayouter implements PopLayouter
 {
     @Override
     public void layout(int x, int y, View popView, View popViewParent)
     {
-        final int parentSize = popViewParent.getHeight();
+        final int parentSize = popViewParent.getWidth();
         if (parentSize <= 0)
         {
             return;
@@ -16,34 +16,34 @@ public class AutoHeightLayouter implements PopLayouter
 
         int consume = 0;
 
-        final int bottom = popView.getBottom();
-        if (bottom > parentSize)
+        final int right = popView.getWidth();
+        if (right > parentSize)
         {
-            consume += (bottom - parentSize);
+            consume += (right - parentSize);
         }
 
-        final int top = popView.getTop();
-        if (top < 0)
+        final int left = popView.getLeft();
+        if (left < 0)
         {
-            consume += (-top);
+            consume += (-left);
         }
 
         final ViewGroup.LayoutParams params = popView.getLayoutParams();
-        int size = params.height;
+        int size = params.width;
         if (consume > 0)
         {
-            size = popView.getHeight() - consume;
+            size = popView.getWidth() - consume;
         } else
         {
-            if (top > 0 && bottom < parentSize)
+            if (left > 0 && right < parentSize)
             {
                 size = ViewGroup.LayoutParams.WRAP_CONTENT;
             }
         }
 
-        if (params.height != size)
+        if (params.width != size)
         {
-            params.height = size;
+            params.width = size;
             popView.setLayoutParams(params);
         }
     }
