995404d9955c847560bc6afb6580845061917c78
zjun <565061763@qq.com>
2021-06-10 17:29:47 +0800

    CombineLayouter

--- lib/src/main/java/com/sd/lib/poper/layouter/CombineLayouter.java
+++ lib/src/main/java/com/sd/lib/poper/layouter/CombineLayouter.java
@@ -9,11 +9,7 @@ import com.sd.lib.poper.Poper;
 public class CombineLayouter implements Poper.Layouter {
     private final Poper.Layouter[] mLayouters;
 
-    public CombineLayouter(Poper.Layouter... layouters) {
-        if (layouters == null || layouters.length <= 0) {
-            throw new IllegalArgumentException("layouters is null or empty");
-        }
-
+    public CombineLayouter(@NonNull Poper.Layouter... layouters) {
         mLayouters = layouters;
     }
 
